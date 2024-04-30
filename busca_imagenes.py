323c8676d179662bc604c9b7c6be76f068bfe8b3
AlemolAMG <amolero@comaferr.es>
2024-04-30 11:30:23 +0200

    Versión 1.0 del proyecto busca_imagenes Funciona para buscar todas las imágenes que existan en el csv.

--- busca_imagenes.py
+++ busca_imagenes.py
@@ -11,16 +11,16 @@ def copiar_imagenes(csv, carpeta_imagenes, carpeta_destino):
 
     # Leer el archivo CSV
     with open(csv, mode='r', encoding='utf-8') as file:
-        cabecera = file.readline().strip()
+        file.readline().strip()
         # next(cabecera)  # Saltar la primera fila si contiene encabezados
 
         for linea in file:
-            id_producto = (linea.split(";")[0])[1:]  # Suponiendo que el ID del producto está en la primera columna
+            id_producto = (linea.split(";")[3].strip())  # Suponiendo que el ID del producto está en la primera columna
             nombre_imagen = f"{id_producto}.jpg"  # Nombre de archivo de la imagen
             
             # Comprobar si la imagen existe en la carpeta de imágenes
             ruta_imagen_origen = os.path.join(carpeta_imagenes, nombre_imagen)
-            # print (f"imagen: {nombre_imagen} - ruta {ruta_imagen_origen}")
+            print (f"imagen: {nombre_imagen} - ruta {ruta_imagen_origen}")
 
             if os.path.exists(ruta_imagen_origen):
                 # Copiar la imagen a la carpeta de destino
@@ -29,13 +29,16 @@ def copiar_imagenes(csv, carpeta_imagenes, carpeta_destino):
 
                 # Contamos la imagen como encontrada.
                 cont_imgs_encontradas = cont_imgs_encontradas + 1
-                print(f"Imagen para el producto {id_producto} copiada correctamente.")
+                print(f"Imagen para el producto {id_producto} copiada correctamente")
             else:
                 # Imagen no encontrada.
                 cont_imgs_falladas = cont_imgs_falladas + 1
-                print(f"No se encontró imagen para el producto {id_producto}.")
+                print(f"No se encontró imagen para el producto {id_producto}")
 
     print(f"Se han encontrado {cont_imgs_encontradas} imágenes, y han fallado {cont_imgs_falladas} imágenes.")        
 
+# Comienzo del programa.
+carpeta: str = "."
+
 # Llamar a la función para copiar imágenes
-copiar_imagenes(csv= "articulos.csv", carpeta_imagenes= ".", carpeta_destino= "EXPORTAR")
+copiar_imagenes(csv= "articulos.csv", carpeta_imagenes= carpeta, carpeta_destino= "EXPORTAR")
